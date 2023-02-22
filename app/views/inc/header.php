d018de4c1e2e6b3254909e0f4ea85c3e2e07b798
raxid bou4nja <rachid.boughnja@gmail.com>
2023-02-22 03:10:13 +0000

    change index page and header

---
+++ app/views/inc/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/fontawesome.min.css">
+  <link href="//db.onlinewebfonts.com/c/5d1d6d747b45d6aff710054b95e79a1f?family=HP+Simplified" rel="stylesheet" type="text/css"/>
+  <title><?php echo SITENAME; ?></title>
+  <style type="text/css">
+    .btn-font-bronze{
+      font-size: 26px;
+      color: #b68869 !important;
+      padding: 8px 33px;
+      border-radius: 7px;
+      box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important;
+    }
+    .infos{
+      position: absolute;
+      font-family: HP Simplified;
+      font-size: 53px;
+      font-weight: bolder;
+    }
+    }
+  </style>
+</head>
+<body>
+  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+  <div class="container">
+  
---
+++ app/views/inc/navbar.php
@@ -0,0 +1,7 @@
+<div class="container">  
+    <nav class="navbar mb-3 d-flex justify-content-start h1">
+      <div class="font-italic" href="<?php echo URLROOT; ?>">
+        <i class="fa fa-bars mr-2"></i><?php echo SITENAME; ?>
+      </div>
+      </nav>
+</div>
