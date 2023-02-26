d10385419e7f399da97c583bb4ae5aa11ae59b37
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2023-02-26 23:09:11 +0100

    Update how_to_play.php

--- app/views/pages/how_to_play.php
+++ app/views/pages/how_to_play.php
@@ -14,7 +14,7 @@
     		<div class="col-md-9">
                 <h1 class="text-center text-white bg-dark rounded p-3 shadow">HOW TO PLAY</h1>
                 <div class="how_to p-2">
-                    <h2 class="p-2 bg-info"><strong>FIRST STEP:</strong> Select A Game And Purchase A Packet Of Tickets</h2>
+                    <h2 class="p-2 bg-info"><strong>FIRST STEP:</strong> Select A Game And Purchase A Pack Of Tickets</h2>
                     <img src="<?php echo URLROOT; ?>/img/how_to_play/titurial_3.png" width="100%">
                 </div>
                 <div class="how_to p-2">
