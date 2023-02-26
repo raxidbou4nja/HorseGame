fbf9b6dcdac7f75a0fc5b3a7377dc241d36d2a21
raxid bou4nja <rachid.boughnja@gmail.com>
2023-02-26 20:01:15 +0000

    rename url_helper to base_helper

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
   // Load Config
   require_once 'config/config.php';
-  require_once 'helpers/url_helpers.php';
+  require_once 'helpers/base_helpers.php';
   require_once 'helpers/session_helper.php';
 
   // Autoload Core Libraries
